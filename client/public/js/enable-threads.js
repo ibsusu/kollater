8a50720ff5f8f302f348b1dfea46fec9f7411a9e
大北 <>
2024-05-28 10:04:02 -0700

    adding all outstanding files

--- client/public/js/enable-threads.js
+++ client/public/js/enable-threads.js
@@ -36,8 +36,10 @@ if(typeof window === 'undefined') {
     }
 
     const headers = new Headers(r.headers);
-    headers.set("Cross-Origin-Embedder-Policy", "credentialless"); // or: require-corp
+    headers.set("Cross-Origin-Embedder-Policy", "require-corp"); // or: require-corp
     headers.set("Cross-Origin-Opener-Policy", "same-origin");
+    headers.set("Cross-Origin-Resource-Policy", "cross-origin");
+
     
     return new Response(r.body, { status: r.status, statusText: r.statusText, headers });
   }
